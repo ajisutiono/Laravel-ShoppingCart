10611a9ff4289dc3ba01aab6b6d20de69406621a
Aji Sutiono <raisadajiwa@gmail.com>
2025-01-03 17:37:29 +0700

    make page cart, add rout to page cart

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Product\Index;
+use App\Livewire\Shop\Cart;
 use App\Livewire\Shop\Index as ShopIndex;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,6 @@ Route::get('/admin/product', Index::class)
 
 Route::get('/shop', ShopIndex::class)
     ->name('shop.index');
+
+Route::get('/cart', Cart::class)
+    ->name('shop.cart');
